bff08ee831233f1ca0db5050946ae8df0947c02e
Psng Abu <abupsngdscz@gmail.com>
2024-03-11 07:21:05 +0530

    user and events model are create and connection to the mongo db also setup

---
+++ lib/database/models/eventModel.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, models, Types } from "mongoose";
+
+export type IEvent = Document & {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category?: { _id: string; name: string };
+  organizer?: { _id: string; firstName: string; lastName: string };
+};
+
+const EventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const EventModel = models.Event || mongoose.model("Event", EventSchema);
+
+export default EventModel;
